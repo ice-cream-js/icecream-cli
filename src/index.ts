243336e832d2b3977b1c7e03364300d199c6623d
MyferIsADev <contactme.myfer@protonmail.com>
2024-05-31 10:58:38 -0500

    🦾v1.1.6

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-const { exec } = require('child_process');
+import chalk from 'chalk';
 
-exec('npm install inquirer', (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
-  
-    console.log(`${stdout}`);
-});
-exec('npm install chalk', (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
-  
-    console.log(`${stdout}`);
-});
-exec('npm install shelljs', (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
-  
-    console.log(`${stdout}`);
-});
+console.log(`${chalk.magenta('Ice Cream')} is a JavaScript library to render HTML and CSS easily.`)
+console.log(`\nUsage: icecream <command> ${chalk.blue('[--flags]')} [...args]`);
+console.log(`\n\nCommands:`)
+console.log(`  ${chalk.magenta("build")}       Install the necessary dependencies to run this CLI.`)
+console.log(`  ${chalk.magenta("make")}       ${chalk.gray("--typescript projectName")}       Make a project using Ice Cream JS.`)
