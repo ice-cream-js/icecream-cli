722365a8b6d4ac04e683a0879d95ec3e2d76d585
MyferIsADev <contactme.myfer@protonmail.com>
2024-07-31 12:34:54 -0500

    Update --build-ts.ts

--- src/commands/--build-ts.ts
+++ src/commands/--build-ts.ts
@@ -3,5 +3,8 @@
 const passedArgs = process.argv.slice(2);
 const shell = require("shelljs");
 
-shell.exec(`tsc ${passedArgs[1]}`)
-shell.exec(`del ${passedArgs[1]}`)
+shell.exec(`tsc ./lib/ice-cream.ts`);
+shell.exec(`del ./lib/ice-cream.ts`);
+
+shell.exec(`tsc ./${passedArgs[0]}`);
+shell.exec(`del ./${passedArgs[0]}`);
